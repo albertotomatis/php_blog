18907ab65a5e63e3a884228fe52a0dc445221441
albertotomatis <hellohako26@gmail.com>
2023-01-19 10:01:07 +0100

    Aggiornata Home page

--- templates/home.php
+++ templates/home.php
@@ -1,8 +1,30 @@
 
         <div class="page-body">
           <div class="container-xl">
+<h1>ISTRUZIONI</h1>
+            <h4>
+            E' scritto in PHP con OOP, senza l'ausilio di framework.<br>
+E' possibile utilizzarlo come modello BACK-END per creare un blog, cambiando solo il front-end.<br><br>
 
-            <p>prova</p>
+Segue il MVC, nella cartella classes c'è un MODELLO (DatabaseTable) per le QUERY al database MYSQL e le Routes.<br>
+Dentro la cartella classes, c'è la cartella CONTROLLER.<br>
+Nella cartella templates ci sono le varie VISTE.<br>
+Inoltre è presente nella cartella includes un file blog.sql contenente le tabelle del database già create.<br><br>
+
+E' possibile effettuare operazioni CRUD ai post, alle categorie e agli autori.<br><br>
+
+E' possibile inoltre assegnare PERMESSI BINARI agli utenti e aggiungere nuovi permessi fino a un massimo di 64.<br><br>
+
+E' presente una funzione di CERCA PER TITOLO riguardante i post.<br><br>
+
+Solo l'admin può REGISTRARE NUOVI AUTORI.<br>
+Eseguire il LOGIN come admin@email.it e inserire come password 123456789<br>
+Le password sono salvate nel db con l'HASH.<br><br>
+
+E' possibile MODIFICARE LA PASSWORD.<br><br>
+
+L'utente loggato accede alla DASHBOARD.
+</h4>
             
           </div>
         </div>
